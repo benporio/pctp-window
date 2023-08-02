10d9aa6464386b4211314c61e418d9d8e6e8c0db
Ben <pot.94.handig@gmail.com>
2023-08-03 02:16:23 +0800

    resolve issue #7

--- res/sql/extract/pricing_extract_qry.sql
+++ res/sql/extract/pricing_extract_qry.sql
@@ -1,10 +1,10 @@
 SELECT 
 --COLUMNS
-    U_BookingNumber, DisableSomeFields, DisableSomeFields2, Code, U_BookingId, U_BookingDate, U_PODNum, U_CustomerName, U_ClientTag, U_ClientProject, U_TruckerName, U_TruckerTag, U_VehicleTypeCap, U_DeliveryStatus,
-    U_TripType, U_NoOfDrops, U_GrossClientRates, U_ISLAND, U_ISLAND_D, U_IFINTERISLAND, U_GrossClientRatesTax, U_RateBasis, U_TaxType, U_GrossProfitNet, U_Demurrage, U_AddtlDrop, U_BoomTruck, U_Manpower, U_Backload,
-    U_TotalAddtlCharges, U_Demurrage2, U_AddtlDrop2, U_BoomTruck2, U_Manpower2, U_Backload2, U_totalAddtlCharges2, U_Demurrage3, U_AddtlCharges, U_GrossProfit, U_TotalInitialClient, U_TotalInitialTruckers, U_TotalGrossProfit,
-    U_ClientTag2, U_GrossTruckerRates, U_GrossTruckerRatesTax, U_RateBasisT, U_TaxTypeT, U_Demurrage4, U_AddtlCharges2, U_GrossProfitC, BillingNum, TPNum, U_ActualBilledRate, U_BillingRateAdjustments,
-    U_BillingActualDemurrage, U_ActualAddCharges, U_TotalRecClients, U_TotalAR, U_VarAR, U_PODSONum, U_ActualRates, U_TPRateAdjustments, U_TPActualDemurrage, U_ActualCharges, U_TPBoomTruck2, U_OtherCharges,
-    U_TotalPayable, U_PVNo, U_TotalAP, U_VarTP, U_APDocNum, U_Paid, U_DocNum, U_DeliveryOrigin, U_Destination, U_RemarksDTR, U_RemarksPOD, U_PODDocNum
+    X.U_BookingNumber, X.DisableSomeFields, X.DisableSomeFields2, X.Code, X.U_BookingId, X.U_BookingDate, X.U_PODNum, X.U_CustomerName, X.U_ClientTag, X.U_ClientProject, X.U_TruckerName, X.U_TruckerTag, X.U_VehicleTypeCap, X.U_DeliveryStatus,
+    X.U_TripType, X.U_NoOfDrops, X.U_GrossClientRates, X.U_ISLAND, X.U_ISLAND_D, X.U_IFINTERISLAND, X.U_GrossClientRatesTax, X.U_RateBasis, X.U_TaxType, X.U_GrossProfitNet, X.U_Demurrage, X.U_AddtlDrop, X.U_BoomTruck, X.U_Manpower, X.U_Backload,
+    X.U_TotalAddtlCharges, X.U_Demurrage2, X.U_AddtlDrop2, X.U_BoomTruck2, X.U_Manpower2, X.U_Backload2, X.U_totalAddtlCharges2, X.U_Demurrage3, X.U_AddtlCharges, X.U_GrossProfit, X.U_TotalInitialClient, X.U_TotalInitialTruckers, X.U_TotalGrossProfit,
+    X.U_ClientTag2, X.U_GrossTruckerRates, X.U_GrossTruckerRatesTax, X.U_RateBasisT, X.U_TaxTypeT, X.U_Demurrage4, X.U_AddtlCharges2, X.U_GrossProfitC, X.BillingNum, X.TPNum, X.U_ActualBilledRate, X.U_BillingRateAdjustments,
+    X.U_BillingActualDemurrage, X.U_ActualAddCharges, X.U_TotalRecClients, X.U_TotalAR, X.U_VarAR, X.U_PODSONum, X.U_ActualRates, X.U_TPRateAdjustments, X.U_TPActualDemurrage, X.U_ActualCharges, X.U_TPBoomTruck2, X.U_OtherCharges,
+    X.U_TotalPayable, X.U_PVNo, X.U_TotalAP, X.U_VarTP, X.U_APDocNum, X.U_Paid, X.U_DocNum, X.U_DeliveryOrigin, X.U_Destination, X.U_RemarksDTR, X.U_RemarksPOD, X.U_PODDocNum
 --COLUMNS
-FROM PRICING_EXTRACT  WITH (NOLOCK) 
+FROM PRICING_EXTRACT X  WITH (NOLOCK) 
