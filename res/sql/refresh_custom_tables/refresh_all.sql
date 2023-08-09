92576f0eedcaa4ebe27622461a56fc18ba69da9f
benedict <pot.94.handig@gmail.com>
2023-08-09 11:20:05 +0800

    resolve issue #9

--- res/sql/refresh_custom_tables/refresh_all.sql
+++ res/sql/refresh_custom_tables/refresh_all.sql
@@ -3229,7 +3229,7 @@ PRINT 'CREATING TMP_UPDATE_TP_EXTRACT'
     ELSE substring(T0.U_PVNo, 3, 100)
     END AS U_PVNo,
 
-    T0.U_ORRefNo,
+    -- T0.U_ORRefNo,
     -- T0.U_ActualPaymentDate,
     -- T0.U_PaymentReference,
     -- T0.U_PaymentStatus,
@@ -3263,6 +3263,13 @@ PRINT 'CREATING TMP_UPDATE_TP_EXTRACT'
     CAST(pod.U_DeliveryReceiptNo as nvarchar(max)) AS U_DeliveryReceiptNo,
     CAST(pod.U_SeriesNo as nvarchar(max)) AS U_SeriesNo,
 
+    SUBSTRING((
+        SELECT
+			CONCAT(', ', T0.U_OR_Ref) AS [text()]
+		FROM OPCH T0 WITH (NOLOCK)
+		WHERE T0.Canceled <> 'Y' AND T0.DocNum IN (SELECT RTRIM(LTRIM(value)) AS DocNum FROM STRING_SPLIT(TF.U_Paid, ','))
+		FOR XML PATH (''), TYPE).value('text()[1]','nvarchar(max)'), 2, 1000
+    ) AS U_ORRefNo,
     SUBSTRING((
         SELECT
             CONCAT(', ', CAST(T0.TrsfrDate AS DATE)) AS [text()]
